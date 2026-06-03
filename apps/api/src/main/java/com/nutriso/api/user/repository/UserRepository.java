93af9efba429e486388a7e8108bbaab5937bca7f
Jakub Cieślik <kuba8172@gmail.com>
2026-06-04 00:01:48 +0200

    feat: user related

---
+++ apps/api/src/main/java/com/nutriso/api/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.nutriso.api.user.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nutriso.api.user.model.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+} 
