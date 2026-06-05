eaf2084c9f3f068d0a1d0c2ca87483ee53298b6a
Jakub Cieślik <kuba8172@gmail.com>
2026-06-05 12:10:48 +0200

    feat: pr-validation tests

--- apps/api/src/test/java/com/nutriso/api/ApiApplicationTests.java
+++ apps/api/src/test/java/com/nutriso/api/ApiApplicationTests.java
@@ -2,9 +2,20 @@ package com.nutriso.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@ActiveProfiles("test")
 @SpringBootTest
 class ApiApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
