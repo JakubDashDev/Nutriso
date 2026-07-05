cc4afc747e8172ded9edd1c218a474aea43cfa74
Jakub Cieślik <kuba8172@gmail.com>
2026-07-05 16:37:01 +0200

    fix: max size of email and password

--- apps/api/src/main/java/com/nutriso/api/auth/dto/LoginRequest.java
+++ apps/api/src/main/java/com/nutriso/api/auth/dto/LoginRequest.java
@@ -4,12 +4,15 @@ import com.nutriso.api.common.exception.ApiErrorCodes;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
   @Email(message = ApiErrorCodes.INVALID_EMAIL)
   @NotBlank(message = ApiErrorCodes.REQUIRED)
+  @Size(max = 254, message = ApiErrorCodes.TOO_LONG_MAX)
   String email,
 
   @NotBlank(message = ApiErrorCodes.REQUIRED)
+  @Size(max = 72, message = ApiErrorCodes.TOO_LONG_MAX)
   String password
 ) {}
